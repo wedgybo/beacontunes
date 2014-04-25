2bb16418c7cc9ad1a8301188f0b9cb0c69b550c8
Jamie Sutherland <jamie@newbridgegreen.com>
2014-04-25 11:26:06 +0100

    Initial Commit

---
+++ beacontunes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  beacontunes
+//
+//  Created by Jamie Sutherland on 25/04/2014.
+//  Copyright (c) 2014 Pussy Galore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ beacontunes/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  beacontunes
+//
+//  Created by Jamie Sutherland on 25/04/2014.
+//  Copyright (c) 2014 Pussy Galore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
